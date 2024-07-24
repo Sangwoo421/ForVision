452fd755666dbd76ee9dbaf41634664e9ec6ae70
boojin <boojin63@gmail.com>
2024-07-24 23:45:48 +0900

    start

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,14 @@
 import { FaCamera } from 'react-icons/fa';
 import '../assets/CSS/style.css'
 
-function Home(){
-    return(
+function Home() {
+    return (
         <div className='HomeContainer'>
-            <FaCamera className='CameraIcon' href="/Camera"/>
-            <a className='text_1'>Touch<br />To<br /> Camera</a>
-            <a className='text_2'>화면을 두 번 터치하세요</a>
+            <div className='HomeContent'>
+                <FaCamera className='CameraIcon' href="/Camera" />
+                <a className='text_1'>Touch<br />To<br /> Camera</a>
+                <a className='text_2'>화면을 두 번 터치하세요</a>
+            </div>
         </div>
     );
 }
