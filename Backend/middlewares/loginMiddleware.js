736edbf1109b7ffda6d704b62896a698cb31d131
Sangwoo421 <asw421@naver.com>
2024-08-25 03:36:40 +0900

    최종

--- Backend/middlewares/loginMiddleware.js
+++
@@ -1,9 +0,0 @@
-const User = require('../models/user');
-
-const loginMiddleware = async (req, res, next) => {
-  const user = await User.findOne({ where: { username: 'testuser' } });
-  req.user = user || null;
-  next();
-};
-
-module.exports = loginMiddleware;
